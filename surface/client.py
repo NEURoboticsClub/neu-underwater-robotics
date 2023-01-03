38a7994bf9c75c35c33b2f8ab6ab95c29894388f
JonahJ27 <jobomcbean@gmail.com>
2023-01-03 15:53:04 -0500

    add drone controll scheme

--- surface/client.py
+++ surface/client.py
@@ -66,26 +66,70 @@ class hat():
 class joystick:
     # we use a list of toggle_vals for all values that behave like toggles not buttons
     # buttons is number of buttons and axes is number of axes
-    def __init__(self, buttons, axes, toggle_vals):
+    def __init__(self, buttons, axes, toggle_vals, trigger_vals, center, radius, ratio):
         self.buttons_dict = {}
         for i in range(buttons):
             if i in toggle_vals:
                 self.buttons_dict[i] = toggle()
             else:
                 self.buttons_dict[i] = button()
-        self.axis_dict = {k:axis(0) for k in range(axes)}
+
+        self.axis_dict = {}
+        for i in range(axes):
+            if i in trigger_vals:
+                self.axis_dict[i] = axis(0)
+            else:
+                self.axis_dict[i] = axis(-1)
+
         self.hat = hat(0, 0)
+        self.center = center
+        self.radius = radius
+        self.ratio = ratio
 
      # get the string to send to robot, format pin:val;
     def get_rov_input(self):
-        output = ""
-        for button in self.buttons_dict:
-            output += f"{button}:{self.buttons_dict[button].get_joy_val()};"
+        # output = ""
+        # for button in self.buttons_dict:
+        #     output += f"{button}:{self.buttons_dict[button].get_joy_val()};"
 
-        output += "\n"
-        for axis in self.axis_dict:
-            output += f"{axis}:{self.axis_dict[axis].get_joy_val()};"
-        return output
+        # output += "\n"
+        # for axis in self.axis_dict:
+        #     output += f"{axis}:{self.axis_dict[axis].get_joy_val()};"
+        # return output
+
+        # max is up right
+        right = self.axis_dict[0].get_joy_val()
+        forward = self.axis_dict[1].get_joy_val() * -1
+        yaw = self.axis_dict[3].get_joy_val()
+        height = self.axis_dict[4].get_joy_val() * -1
+        
+        front_tilt = (self.axis_dict[2].get_joy_val() + 1) / 2
+        back_tilt = (self.axis_dict[5].get_joy_val() + 1) / 2
+
+        precision = self.buttons_dict[0].get_joy_val() + 1.0
+
+        if precision > 1.1:
+            precision = self.ratio
+        # depth_hold = self.buttons_dict[2].get_joy_val() # for auto depth
+
+        front_left = self.radius * precision * (forward + right + yaw) / 3.0 + self.center
+        back_left = self.radius * precision * (forward - right + yaw) / 3.0 + self.center
+        front_right = self.radius * precision * (forward - right - yaw) / 3.0 + self.center
+        back_right = self.radius * precision * (forward + right - yaw) / 3.0 + self.center
+
+        front_vert = max(self.center - self.radius, min(self.center + self.radius, 
+            precision * (self.radius * height + self.radius * front_tilt) + self.center))
+        back_vert = max(self.center - self.radius, min(self.center + self.radius, 
+            precision * (self.radius * height + self.radius * back_tilt) + self.center))
+
+
+        pin_dict = {4: int(front_left), 5: int(front_right), 6: int(back_left), 
+                    7: int(back_right), 8: int(front_vert), 9: int(back_vert)}
+
+        output = ""
+        for pin in pin_dict:
+            output += f"{pin}:{pin_dict[pin]};"
+        return output[:-1]
 
 
     def detect_event(self):
@@ -108,20 +152,20 @@ class joystick:
 
             print(self.get_rov_input())
 
-    def setup(self):
+    def setup(self, joy_num):
         pygame.joystick.init()
         joysticks = [pygame.joystick.Joystick(
             x) for x in range(pygame.joystick.get_count())]
         pygame.init()
-        j = pygame.joystick.Joystick(0)
+        j = pygame.joystick.Joystick(joy_num)
         j.init()
 
 
-j1 = joystick(11, 6, [0, 2])
+j1 = joystick(11, 6, [0, 2], [2, 5], 90, 55, 0.2)
 
 
 
-j1.setup()
+j1.setup(0)
 
 
 while True:
