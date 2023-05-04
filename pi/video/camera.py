48aba042b41c7ca1c55c30c7d9f39bb94b5e7fa4
joshua2020181 <josh.cheng.sh@gmail.com>
2023-05-03 21:17:50 -0400

    change to udpsrc

--- pi/video/camera.py
+++ pi/video/camera.py
@@ -4,7 +4,7 @@ import cv2
 class Camera:
     def __init__(self):
         self.video = cv2.VideoCapture(
-            'v4l2src device=/dev/video0 ! video/x-raw,width=1920,height=1080 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+            'gst-launch-1.0 udpsrc port=5600 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
     
     def get_frame(self):
         ret, image = self.video.read()
