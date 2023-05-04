99ddf58c61e1153f359c8ab60d13e4c1428a7cdb
root <root@Joshua-x360>
2023-05-03 21:59:37 -0400

    working flask server

--- pi/video/camera.py
+++ pi/video/camera.py
@@ -1,16 +1,42 @@
 import cv2
-
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GstApp
 
 class Camera:
     def __init__(self):
-        self.video = cv2.VideoCapture(
-            'gst-launch-1.0 udpsrc port=5600 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
-    
-    def get_frame(self):
-        ret, image = self.video.read()
-        if not ret:
-            print('No image from camera')
-            return
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        self._frame = None
+        Gst.init(None)
+        gst_str = 'udpsrc port=5600 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! video/x-raw,width=1920,height=1080,format=RGB ! appsink name=appsink0'
+        self.pipeline = Gst.parse_launch(gst_str)
+        self.pipeline.set_state(Gst.State.PLAYING)
+        self.appsink = self.pipeline.get_by_name('appsink0')
+        self.appsink.set_property('emit-signals', True)
+        self.appsink.set_property('max-buffers', 1)
+        self.appsink.set_property('drop', True)
+        self.appsink.set_property('sync', False)
+        self.appsink.connect('new-sample', self.on_new_buffer)
+
+    def on_new_buffer(self, sink):
+        sample = sink.emit('pull-sample')
+        buffer = sample.get_buffer()
+        self._frame = np.ndarray(
+            shape=(1080, 1920, 3),
+            buffer=buffer.extract_dup(0, buffer.get_size()),
+            dtype=np.uint8,
+        )
+        return Gst.FlowReturn.OK
+
+    def get_frame(self) -> bytes:
+        if self._frame.any():
+            file_object = BytesIO()
+            img = Image.fromarray(self._frame)
+            img.save(file_object, 'JPEG')
+            file_object.seek(0)
+            return file_object.read()
+        return b''
+
         
