b2122a3c51178a645ff3898ebebe5a838edefeb7
JonahJ27 <jobomcbean@gmail.com>
2023-05-07 14:03:48 -0400

    support for new arm

--- pi/async_server.py
+++ pi/async_server.py
@@ -167,10 +167,10 @@ class Server:
         #         self.board.get_pin(f"d:{i + 1}:o"),
         #     )
         #     tasks.append(self.pins[i].run())
-        for i in range(4, 14):
+        for i in range(4, 12):
             self.pins[i] = Servo(self.board.get_pin(f"d:{i}:s"))
             tasks.append(self.pins[i].run())
-        for i in range(2, 4):
+        for i in [2, 3, 12, 13]:
             self.pins[i] = LinActuator(self.board.get_pin(f"d:{i}:o"))
             tasks.append(self.pins[i].run())
 
