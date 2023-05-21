6c00745ee80483142cebb2fd429b0624106763ef
Joshua Cheng <josh.cheng.sh@gmail.com>
2023-05-21 00:19:29 -0400

    mock

---
+++ surface/video/mock_camera.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from io import BytesIO
+
+
+class Camera:
+    def __init__(self, port):
+        img = Image.open('nuwave.jpg')
+        file_object = BytesIO()
+        img.save(file_object, 'JPEG')
+        file_object.seek(0)
+        self.frame = file_object.read()
+        
+    def get_frame(self) -> bytes:
+        return self.frame
