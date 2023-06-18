d7b5e77cdd15608b0a29cba04f85eba2c206c45d
joshua <josh.cheng.sh@gmail.com>
2023-06-18 16:27:10 -0400

    port and renames

--- pi/video/video_stream.py
+++ pi/video/video_stream.py
@@ -17,22 +17,27 @@ def index():
     return render_template("index.html")
 
 
+INDEX_TO_DEVICE = {
+    "0": "/dev/video0",
+    "1": "/dev/video4",
+}
+
 cameras = {
-    "5600": Camera(5600),
-    # "5601": Camera(5601),
+    "0": Camera(INDEX_TO_DEVICE.get("0")),
+    "1": Camera(INDEX_TO_DEVICE.get("1")),
 }
 
 
-def gen(port):
-    camera = cameras.get(str(port))
+def gen(index):
+    camera = cameras.get(str(index))
     while True:
         frame = camera.get_frame()
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
 
-@app.route("/raw_frame/<port>")
-def raw_frame(port):
-    camera = cameras.get(str(port))
+@app.route("/raw_frame/<index>")
+def raw_frame(index):
+    camera = cameras.get(str(index))
     img = Image.open(BytesIO(camera.get_frame()))
     image_io = BytesIO()
     img.save(image_io, 'PNG')
@@ -40,10 +45,10 @@ def raw_frame(port):
     return render_template('image.html', image_data=dataurl)
 
 
-@app.route("/video_feed/<port>")
-def video_feed(port):
-    return Response(gen(port), mimetype="multipart/x-mixed-replace; boundary=frame")
+@app.route("/video_feed/<index>")
+def video_feed(index):
+    return Response(gen(index), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", use_reloader=False, debug=True, port=8080)
+    app.run(host="0.0.0.0", use_reloader=False, debug=True, port=9000)
