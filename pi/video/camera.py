16033beda0db74af139c3156e1512b3c88232eb8
joshua <josh.cheng.sh@gmail.com>
2023-06-18 14:49:41 -0400

    pi video stream initial commit

---
+++ pi/video/camera.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('GstApp', '1.0')
+from gi.repository import Gst, GstApp
+
+class Camera:
+    def __init__(self, port):
+        self._frame = None
+        Gst.init(None)
+        gst_str = f'v4l2src device=/dev/video2 ! video/x-h264, width=1920,height=1080! h264parse ! queue ! v4l2h264dec ! videoconvert ! videoscale ! video/x-raw,width=1280,height=720,format=RGB ! appsink name=appsink0'
+        print(gst_str)
+        self.pipeline = Gst.parse_launch(gst_str)
+        self.appsink = self.pipeline.get_by_name('appsink0')
+        self.appsink.set_property('emit-signals', True)
+        self.appsink.set_property('max-buffers', 1)
+        self.appsink.set_property('drop', True)
+        self.appsink.set_property('sync', False)
+        self.appsink.connect('new-sample', self.on_new_buffer)
+
+        self.pipeline.set_state(Gst.State.PLAYING)
+
+    def __del__(self):
+        self.pipeline.set_state(Gst.State.NULL)
+
+    def on_new_buffer(self, sink):
+        sample = sink.emit('pull-sample')
+        buffer = sample.get_buffer()
+        self._frame = np.ndarray(
+            shape=(720, 1280, 3),
+            buffer=buffer.extract_dup(0, buffer.get_size()),
+            dtype=np.uint8,
+        )
+        return Gst.FlowReturn.OK
+
+    def get_frame(self) -> bytes:
+        if self._frame is not None and self._frame.any():
+            file_object = BytesIO()
+            img = Image.fromarray(self._frame)
+            img.save(file_object, 'JPEG')
+            file_object.seek(0)
+            return file_object.read()
+        return b''
+
+        
---
+++ pi/video/video_stream.py
@@ -0,0 +1,50 @@
+import time
+
+from flask import Flask, render_template, Response
+
+from io import BytesIO
+from base64 import b64encode
+from PIL import Image
+
+from camera import Camera
+# from mock_camera import Camera
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+cameras = {
+    "5600": Camera(5600),
+    # "5601": Camera(5601),
+}
+
+
+def gen(port):
+    camera = cameras.get(str(port))
+    time.sleep(1)
+    while True:
+        frame = camera.get_frame()
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+
+@app.route("/raw_frame/<port>")
+def raw_frame(port):
+    camera = cameras.get(str(port))
+    img = Image.open(BytesIO(camera.get_frame()))
+    image_io = BytesIO()
+    img.save(image_io, 'PNG')
+    dataurl = 'data:image/png;base64,' + b64encode(image_io.getvalue()).decode('ascii')
+    return render_template('image.html', image_data=dataurl)
+
+
+@app.route("/video_feed/<port>")
+def video_feed(port):
+    return Response(gen(port), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", use_reloader=False, debug=True)
