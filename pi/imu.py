299f8415618301b737bbb384eb2d8c0e119b0c05
kasi-mov <ditteisak@gmail.com>
2023-11-12 15:13:21 -0500

    created client for imu

--- pi/imu.py
+++ pi/imu.py
@@ -1,45 +1,22 @@
-import logging
-import sys
 import time
+import json
+import socket
 
 from Adafruit_BNO055 import BNO055
 
+HOST = "192.168.0.114"  # The server's hostname or IP address
+PORT = 2049  # The port used by the server
+CONTROL_LOOP_FREQ = 100  # Hz
+
 bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
 
-
-# Enable verbose debug logging if -v is passed as a parameter.
-if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
-    logging.basicConfig(level=logging.DEBUG)
-
-# Initialize the BNO055 and stop if something went wrong.
-if not bno.begin():
-    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
-
-# Print system status and self test result.
-status, self_test, error = bno.get_system_status()
-print('System status: {0}'.format(status))
-print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
-# Print out an error if system status is in error mode.
-if status == 0x01:
-    print('System error: {0}'.format(error))
-    print('See datasheet section 4.3.59 for the meaning.')
-
-# Print BNO055 software revision and other diagnostic data.
-sw, bl, accel, mag, gyro = bno.get_revision()
-print('Software version:   {0}'.format(sw))
-print('Bootloader version: {0}'.format(bl))
-print('Accelerometer ID:   0x{0:02X}'.format(accel))
-print('Magnetometer ID:    0x{0:02X}'.format(mag))
-print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
-
-print('Reading BNO055 data, press Ctrl-C to quit...')
-while True:
+def read_data():
     # Read the Euler angles for heading, roll, pitch (all in degrees).
     heading, roll, pitch = bno.read_euler()
     # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
     sys, gyro, accel, mag = bno.get_calibration_status()
     # Print everything out.
-    print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
+    return ('Heading: {0:0.2F} Roll: {1:0.2F} Pitch: {2:0.2F}\tSys_cal: {3} Gyro_cal: {4} Accel_cal: {5} Mag_cal: {6}'.format(
           heading, roll, pitch, sys, gyro, accel, mag))
     # Other values you can optionally read:
     # Orientation as a quaternion:
@@ -58,5 +35,33 @@ while True:
     # Gravity acceleration data (i.e. acceleration just from gravity--returned
     # in meters per second squared):
     #x,y,z = bno.read_gravity()
-    # Sleep for a second until the next reading.
-    time.sleep(1)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print(f"connecting to {HOST}:{PORT}")
+    s.connect((HOST, PORT))
+    last_time = time.time()
+
+    # Initialize the BNO055 and stop if something went wrong.
+    if not bno.begin():
+        raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+    
+    # Print out an error if system status is in error mode.
+    status, self_test, error = bno.get_system_status()
+    if status == 0x01:
+        print('System error: {0}'.format(error))
+        print('See datasheet section 4.3.59 for the meaning.')
+        
+    while True:
+        data = read_data()
+        msg = {
+            "imu data": json.dumps(data.to_dict()),
+        }
+        s.send(str.encode(json.dumps(msg)))
+        print(f"sent: {msg}")
+
+        # sleep for remainder of loop
+        if time.time() - last_time < 1 / CONTROL_LOOP_FREQ:
+            time.sleep(1 / CONTROL_LOOP_FREQ - (time.time() - last_time))
+        else:
+            print("Warning: control loop took too long")
+        last_time = time.time()
