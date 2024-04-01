96b6a6bfb1d659336b4be488e4ab7c8a763f7d88
NEU Robotics MATE pi <neumate2022@gmail.com>
2024-03-31 20:17:35 -0400

    neo forgot how github works and didn't add all of the files last time

--- pi/depth-sensor.py
+++ pi/depth-sensor.py
@@ -4,19 +4,19 @@ import os
 import socket
 import time
 
-HOST = "192.168.0.102"  # The server's hostname or IP address
+HOST = "192.168.0.116"  # The server's hostname or IP address
 PORT = 2049  # The port used by the server
 CONTROL_LOOP_FREQ = 100  # Hz
 
 # Print readings
-def read_depth(self, sensor):        
+def read_depth(sensor):        
         if sensor.read():
-                print(("P: %0.1f mbar  %0.3f psi\tDepth: %0.3f\tT: %0.2f C  %0.2f F") % 
+                print(("P: %0.1f mbar  %0.3f psi\tDepth: %0.3f\tT: %0.2f C  %0.2f F") % (
                 sensor.pressure(), # Default is mbar (no arguments)
                 sensor.pressure(ms5837.UNITS_psi), # Request psi
                 sensor.depth(),
                 sensor.temperature(), # Default is degrees C (no arguments)
-                sensor.temperature(ms5837.UNITS_Farenheit)) # Request Farenheit
+                sensor.temperature(ms5837.UNITS_Farenheit))) # Request Farenheit
         else:
                 print("Sensor read failed!")
                 exit(1)
@@ -25,7 +25,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(f"connecting to {HOST}:{PORT}")
     s.connect((HOST, PORT))
     last_time = time.time()
-    sensor = ms5837.MS5837_02BA(21) # Default I2C bus is 1 (Raspberry Pi 3)
+    sensor = ms5837.MS5837_02BA(1) # Default I2C bus is 1 (Raspberry Pi 3)
 
     # We must initialize the sensor before reading it
     if not sensor.init():
@@ -57,7 +57,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             depth_list.pop(0)
 
         msg = {
-            "depth": json.dumps(depth_list.to_dict()),
+            "depth": json.dumps(depth_list),
         }
         s.send(str.encode(json.dumps(msg)))
         print(f"sent: {msg}")
