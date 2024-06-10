7b3e2eac7cc2e44ba4b15b3a8e765ffd06d62d53
NEU Robotics MATE pi <neumate2022@gmail.com>
2024-06-10 15:46:34 -0400

    depth sensor test

--- surface/video/gui/gui_app.py
+++ surface/video/gui/gui_app.py
@@ -71,7 +71,7 @@ def single_view(camera_id):
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=3000, debug=True)
+    app.run(host="localhost", port=3000, debug=False)
 
 
 # sends camera bytes as a file 
