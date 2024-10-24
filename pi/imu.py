47287b4fbb22f7c53b978c11c5730412c171675b
neumate2022 <neumate2022@gmail.com>
2024-10-23 21:20:54 -0400

    i2c

--- pi/imu.py
+++ pi/imu.py
@@ -15,17 +15,17 @@ from adafruit_bno08x.i2c import BNO08X_I2C
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 bno = BNO08X_I2C(i2c)
 
-bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+# bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 bno.enable_feature(BNO_REPORT_GYROSCOPE)
 bno.enable_feature(BNO_REPORT_MAGNETOMETER)
 bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
 while True:
     time.sleep(0.5)
-    print("Acceleration:")
-    accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
-    print("")
+    # print("Acceleration:")
+    # accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
+    # print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
+    # print("")
 
     print("Gyro:")
     gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
