2bf8373bd4571dde3ee1673e4e608fbfa8b490f7
melodicht <marvincht@gmail.com>
2024-12-29 21:41:42 -0800

    Clean up video player widget module

--- surface/video/widgets/video_player.py
+++ surface/video/widgets/video_player.py
@@ -4,24 +4,41 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtCore import QUrl
 
 class VideoPlayerWidget(QWidget):
-    """
-    A PyQt5 Widget that plays a video with the given URL.
+    """A PyQt5 Widget that plays a video with the given URL.
 
     This is best understood as a convenience class for combining
     QVideoWidget with QMediaPlayer.  While there are no methods, the
     class needs to inherit from QWidget.
 
+    For documentation on the interactions between GST, the video
+    widget and the media player, see
+    https://doc.qt.io/qt-5/qmediaplayer.html#setMedia
+
+    By accepting a QUrl, this widget expects that its clients assemble
+    the QUrl. In the future, we may find that we prefer to have the
+    parsing done within this widget.
+
     """
-    def __init__(self, qurl, video_widget, parent=None):
+    def __init__(self, qurl, parent=None):
+        """Constructs a new video player widget.
+        
+        qurl : QUrl -- To be passed into a QMediaContent, and is set
+        to the internal media player.
+        
+        parent : QWidget?
+
+        """
         super(VideoPlayerWidget, self).__init__(parent)
 
-        self.media_player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
-        self.video_widget = video_widget
-        #self._set_layout_to_given(self.video_widget)
-        self.media_player.setVideoOutput(self.video_widget)
-        self.media_player.setMedia(QMediaContent(qurl))
-        self.video_widget.show()
-        self.media_player.play()
+        media_player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
+        media_player.mediaStatusChanged.connect(self.on_media_changed)
+        media_player.error.connect(self.on_state_changed)
+        video_widget = QVideoWidget(self)
+        self._set_layout_to_given(video_widget)
+        media_player.setVideoOutput(video_widget)
+        media_content = QMediaContent(qurl)
+        media_player.setMedia(media_content)
+        media_player.play()
 
     def _set_layout_to_given(self, w):
         """Sets the layout of this widget to the given widget.
@@ -32,22 +49,18 @@ class VideoPlayerWidget(QWidget):
         layout.addWidget(w)
         self.setLayout(layout)
 
-from PyQt5 import QtWidgets
+    def on_media_changed(self, new_media):
+        print(f"Media changed to: {new_media}")
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        qurl = QUrl("gst-pipeline: udpsrc port=5000 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! xvimagesink name=\"qtvideosink\"")
-        video_widget = QVideoWidget(self)
-        player = VideoPlayerWidget(qurl, video_widget, parent=self)
-        self.setCentralWidget(video_widget)
-        
-        
+    def on_state_changed(self, new_state):
+        print(f"State changed to: {new_state}")
+
 if __name__ == '__main__':
     import sys
     from PyQt5.QtWidgets import QApplication
 
     app = QApplication(sys.argv)
-    mw = MainWindow()
-    mw.show()
+    qurl = QUrl("gst-pipeline: udpsrc port=5000 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! xvimagesink name=\"qtvideosink\"")
+    player = VideoPlayerWidget(qurl)
+    player.show()
     sys.exit(app.exec_())
