17c784d8ae3afccd83f7194488580e12029b906a
melodicht <marvincht@gmail.com>
2024-12-30 21:23:06 -0800

    Tidy up video player widget and make widgets folder a module

--- surface/video/widgets/video_player.py
+++ surface/video/widgets/video_player.py
@@ -4,7 +4,10 @@ from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtCore import QUrl
 
 class VideoPlayerWidget(QWidget):
-    """A PyQt5 Widget that plays a video with the given URL.
+    """A PyQt5 Widget that plays a video with the given QUrl.
+
+    If the media player is unable to play with the given QUrl, an
+    generic exception is raised with the error state value.
 
     This is best understood as a convenience class for combining
     QVideoWidget with QMediaPlayer.  While there are no methods, the
@@ -15,29 +18,38 @@ class VideoPlayerWidget(QWidget):
     https://doc.qt.io/qt-5/qmediaplayer.html#setMedia
 
     By accepting a QUrl, this widget expects that its clients assemble
-    the QUrl. In the future, we may find that we prefer to have the
+    the QUrl.  In the future, we may find that we prefer to have the
     parsing done within this widget.
-
     """
-    def __init__(self, qurl, parent=None):
+    def __init__(self, qurl, parent=None, on_media_status_changed=None, on_error=None):
         """Constructs a new video player widget.
-        
+
         qurl : QUrl -- To be passed into a QMediaContent, and is set
         to the internal media player.
-        
+
         parent : QWidget?
 
+        on_media_status_changed : [MediaStatus -> Void]?
+
+        on_error : [QMediaPlayer.Error -> Void]?
         """
         super(VideoPlayerWidget, self).__init__(parent)
 
+        # Create media player, and attach listeners
         media_player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
-        media_player.mediaStatusChanged.connect(self.on_media_changed)
-        media_player.error.connect(self.on_state_changed)
+        if on_media_status_changed:
+            media_player.mediaStatusChanged.connect(on_media_status_changed)
+        if on_error:
+            media_player.error.connect(on_error)
+        media_player.error.connect(self.on_error)
+
+        # Video widget
         video_widget = QVideoWidget(self)
         self._set_layout_to_given(video_widget)
+
+        # Finish setting up media player
         media_player.setVideoOutput(video_widget)
-        media_content = QMediaContent(qurl)
-        media_player.setMedia(media_content)
+        media_player.setMedia(QMediaContent(qurl))
         media_player.play()
 
     def _set_layout_to_given(self, w):
@@ -49,18 +61,14 @@ class VideoPlayerWidget(QWidget):
         layout.addWidget(w)
         self.setLayout(layout)
 
-    def on_media_changed(self, new_media):
-        print(f"Media changed to: {new_media}")
+    def on_error(self, new_error):
+        """Watches for changes to the error state, and raises an
+        exception if there is one.
 
-    def on_state_changed(self, new_state):
-        print(f"State changed to: {new_state}")
+        new_error : QMediaPlayer.Error
+        """
+        # TODO(error): Ought to make a custom exception, Exception is
+        # too generic.
+        if new_error != 0:
+            raise Exception(f"Media player error state: {new_error}")
 
-if __name__ == '__main__':
-    import sys
-    from PyQt5.QtWidgets import QApplication
-
-    app = QApplication(sys.argv)
-    qurl = QUrl("gst-pipeline: udpsrc port=5000 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! videoconvert ! xvimagesink name=\"qtvideosink\"")
-    player = VideoPlayerWidget(qurl)
-    player.show()
-    sys.exit(app.exec_())
