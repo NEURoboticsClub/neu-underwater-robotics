61036f63ad3bf973b415991e063b35f10d8cdb04
kasi-mov <ditteisak@gmail.com>
2025-06-09 02:05:21 -0400

    initial attempt to fix unholy amounts of latency

--- surface/gui/widgets/camera_feed.py
+++ surface/gui/widgets/camera_feed.py
@@ -40,7 +40,7 @@ class CameraFeed(QThread):
                 self.frame_signal.emit(img)
                 if self._do_save_img:
                     self._save_image()
-            time.sleep(0.01)
+            time.sleep(0.05)
         print("Error: Camera closed. Exiting.")
     
     def _cvimage_to_qimage(self, frame) -> QImage:
