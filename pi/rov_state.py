1f51a9d99a10181fded54e9d4687c693f910b127
miayim <miayim@gmail.com>
2025-06-13 00:18:22 -0400

    make rate tiny for testing

--- pi/rov_state.py
+++ pi/rov_state.py
@@ -54,7 +54,7 @@ class ROVState:
         self._slew_limiters = {}
         for name, thruster in self.thrusters.items():
             self._slew_limiters[name] = SlewRateLimiter(
-                max_rate=(thruster.active_range[1] - thruster.active_range[0]) # max rate of change per second
+                max_rate=((thruster.active_range[1] - thruster.active_range[0]) / 10) # max rate of change per second
             )
 
         # for axis in self._current_velocity.keys():
