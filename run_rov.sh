665cc8f112920231440537daf5f2fbe1bb88242c
kasi-mov <ditteisak@gmail.com>
2025-06-14 17:24:41 -0400

    fix run_rov.sh to accept sigint

--- run_rov.sh
+++ run_rov.sh
@@ -1,7 +1,16 @@
-     #!/bin/bash
+#!/bin/bash
 
-     # run the async server and all clients
-     python -m pi.async_server &
-     python -m pi.imu &
-     python -m pi.depth-sensor &
-     wait
+trap 'killall' INT
+
+killall() {
+    trap '' INT TERM # ignore INT and TERM while shutting down
+    kill -TERM 0
+    wait # wait for child processes to complete
+    echo DONE
+}
+
+# run the async server and all clients
+python -m pi.async_server &
+python -m pi.imu &
+python -m pi.depth-sensor &
+cat # wait forever
