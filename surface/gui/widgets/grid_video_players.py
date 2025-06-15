78d07d1ce9720eddfba05da4e770ba0b283b9f46
kasi-mov <ditteisak@gmail.com>
2025-06-14 21:45:53 -0400

    neo fixes their mistakes

--- surface/gui/widgets/grid_video_players.py
+++ surface/gui/widgets/grid_video_players.py
@@ -37,9 +37,9 @@ class GridVideoPlayersWidget(QWidget):
         self.grid.setSpacing(0)
         self.grid.setContentsMargins(0, 0, 0, 0)
 
-        for i, port_no in enumerate(port_nums):
+        for i, _ in enumerate(port_nums):
             os.system(f"sudo modprobe v4l2loopback video_nr={i}")
-            os.system(PORT_NUM_TO_GST_PIPELINE_COMMAND(port_no))
+            os.system(PORT_NUM_TO_GST_PIPELINE_COMMAND(i))
         
         self._video_players = [VideoPlayerWidget(port, i) for i, port in enumerate(port_nums)]
 
